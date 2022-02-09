cf3c360ec3b0677241c0dda441a4102f6f538153
Yuriy Kuptsov <msvyatoy@gmail.com>
2022-02-08 23:37:02 -0500

    initial repo setup complete

---
+++ src/styles/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  color: red;
+  background-color: blue;
+`
+export const Button = styled.button`
+  color: blue;
+  border: 2px solid black
+`
