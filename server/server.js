b95324135e58d278a0848293827d149303bd2249
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-02-14 11:54:33 -0600

    Implement get request to set products state on App.jsx

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const port = 3000;
+const config = require('../config.js');
+const axios = require('axios');
 
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-  res.send('Cat Walk Project');
+app.get('/API/products', (req, res) => {
+  console.log('Request:', req.body)
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',
+   {headers: {Authorization: config.TOKEN}})
+  .then((response) => {
+    res.send(response.data).status(200);
+  })
+  .catch((error) => {
+    // throw new Error(error);
+    // console.log(error);
+  })
 })
 
 app.listen(port, () => {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
 import { Title, Button } from '../styles/styles';
-
+import ReviewList from './Reviews/ReviewList.jsx';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      products: []
     }
+    this.getProducts = this.getProducts.bind(this);
   }
 
+  componentDidMount () {
+    this.getProducts();
 
+  }
+
+  getProducts() {
+    axios.get('/API/products')
+    .then((response) => {
+      // console.log('Reponse:', response.data)
+      this.setState({
+        products: response.data
+      })
+      console.log('Products', this.state.products)
+    })
+    .catch((error) => {
+      throw new Error(error);
+    })
+  }
 
   render() {
     return (
@@ -18,6 +37,7 @@ class App extends Component {
 
         <Title>Project Cat Walk</Title>
         <Button>button</Button>
+        <ReviewList products={this.state.products}/>
       </div>
     )
   }
