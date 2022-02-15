57bf845f92227c00d472e6809e2054efba6260d5
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-02-15 13:24:55 -0600

    Implement dynamic API calls to review based off product id

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Title, Button } from '../styles/styles';
-// import ReviewList from './Reviews/ReviewList.jsx';
+import ReviewListv1 from './Reviews/ReviewListv1.jsx';
 import axios from 'axios';
 
 class App extends Component {
@@ -13,24 +13,26 @@ class App extends Component {
     this.getProducts = this.getProducts.bind(this);
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.getProducts();
   }
 
   getProducts() {
     axios.get('/API/products')
-    .then((response) => {
-      this.setState({
-        products: response.data,
-        product: response.data[0]
+      .then((response) => {
+        console.log()
+        this.setState({
+          products: response.data,
+          product: response.data[0]
+        })
       })
-    })
-    .catch((error) => {
-      throw new Error(error);
-    })
+      .catch((error) => {
+        throw new Error(error);
+      })
   }
 
   render() {
+    // console.log('blah', this.state.product)
     return (
       <div>
         <Title>Project Cat Walk</Title>
@@ -38,7 +40,7 @@ class App extends Component {
         {/* <Overview products={this.state.products} product={this.state.product} /> */}
         {/* <RelatedProducts products={this.state.products} product={this.state.product} /> */}
         {/* <QnA products={this.state.products} product={this.state.product} /> */}
-        {/* <ReviewList products={this.state.products} product={this.state.product} /> */}
+        {Object.keys(this.state.product).length && this.state.products.length ? <ReviewListv1 products={this.state.products} product={this.state.product} /> : null}
       </div>
     )
   }
