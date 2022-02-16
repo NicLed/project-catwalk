c17c17ad1b2a31d3ed671e43fc8cc4563bfc4219
Nicholas Ledermann <nic.ledermann@gmail.com>
2022-02-15 18:22:58 -0600

    Refactor App.jsx to use hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Title, Button } from '../styles/styles';
 import RatingsReviews from './Reviews/RatingsReviews.jsx';
 import axios from 'axios';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: [],
-      product: {}
-    }
-    this.getProducts = this.getProducts.bind(this);
-  }
+const App = (props) => {
+  const [products, setProducts] = useState([])
+  const [product, setProduct] = useState({})
 
-  componentDidMount() {
-    this.getProducts();
-  }
+  useEffect(() => {
+    getProducts();
+  }, [])
 
-  getProducts() {
+  const getProducts = () => {
     axios.get('/API/products')
       .then((response) => {
-        console.log()
-        this.setState({
-          products: response.data,
-          product: response.data[0]
-        })
+        setProducts(response.data);
+        setProduct(response.data[0]);
       })
       .catch((error) => {
         throw new Error(error);
       })
   }
 
-  render() {
-    // console.log('blah', this.state.product)
-    return (
-      <div>
-        <Title>Project Cat Walk</Title>
-        <Button>button</Button>
-        {/* <Overview products={this.state.products} product={this.state.product} /> */}
-        {/* <RelatedProducts products={this.state.products} product={this.state.product} /> */}
-        {/* <QnA products={this.state.products} product={this.state.product} /> */}
-        {Object.keys(this.state.product).length && this.state.products.length ? <RatingsReviews products={this.state.products} product={this.state.product} /> : null}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Title>Project Cat Walk</Title>
+      <Button>button</Button>
+      {/* <Overview products={products} product={product} /> */}
+      {/* <RelatedProducts products={products} product={product} /> */}
+      {/* <QnA products={products} product={product} /> */}
+      {/* {Object.keys(product).length && products.length ?
+        <RatingsReviews products={products} product={product} />
+        : null} */}
+    </div>
+  )
 }
 
 export default App;
